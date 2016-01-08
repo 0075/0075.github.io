ccc4380efccdb96cd76875b9963bf289f4a9c346
0075 <viktor.hagstrom@outlook.com>
2016-01-08 22:34:52 +0100

    The start of a beginning

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<!-- PHP -->
+<?php
+$top_margin = "5%";
+$title = "Snail";
+
+?>
+
+<html lang="en">
+
+<head>
+
+  <!-- Basic Page Needs -->
+  <meta charset="utf-8">
+  <title>
+    <?php echo $title ?>
+  </title>
+  <meta name="description" content="hgstrm.se is a simple website where I can train my programming skills and make some fun stuff!">
+  <meta name="author" content="Viktor Hagström">
+
+  <!-- Mobile Specific Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'> <!-- Normal text -->
+  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'> <!-- <h1> text -->
+  <link href='https://fonts.googleapis.com/css?family=Droid+Sans+Mono' rel='stylesheet' type='text/css'> <!-- Code -->
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/normalize.css" type="text/css">
+  <link rel="stylesheet" href="css/skeleton.css" type="text/css">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+<!-- Java Script -->
+<script>
+
+</script>
+
+
+<body>
+
+  <!-- Primary Page Layout -->
+  <!-- –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+  <div class="container">
+    <div class="row">
+      <div class="column" style="margin-top: <?php echo $top_margin ?>" id="header">
+        <h1 id="title" style="center">
+          hgstrm
+        </h1>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!-- End Document -->
+
+</body>
+
+</html>
